a1694c2008fe13b61c6ef4e12b9faece2751cbf3
Rye <reynaldo.ona@starfi.sh>
2013-05-08 14:18:45 +0800

    imat's work merged here

--- Project/Design/1_Website/Main_Layout/templates/global_inpage_javascript.js
+++ Project/Design/1_Website/Main_Layout/templates/global_inpage_javascript.js
@@ -3,5 +3,18 @@ $(document).ready(function(){
 	Cufon.replace('#primary_nav li a,#menu_subNav li a,#downloadMenu,#forReserve .fs-xs,p', { fontFamily: 'HelveticaNeueLT Com 45 Lt' });
 	Cufon.replace('#homeBanner p, #forReserve .fs-s', { fontFamily: 'Helvetica Neue LT Std 55' });
 	Cufon.replace('.menuList h1,.menuList h3,.menuList p', { fontFamily: 'Goudy Old Style' });
+	
+	
+	
+	//ajax for adding subscriber
+	$("input[name='subscriber']").click(function(e){
+		e.preventDefault();
+		email = $("input[name='email']").val();
+		$.php('/ajax/home/addSubscriber', {email:email});	
+		
+		php.complete = function(){
+			$("input[name='email']").val('');
+		}
+	});
 });
 
